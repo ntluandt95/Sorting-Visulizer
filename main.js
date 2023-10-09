d0811473223bc91d2a0140910d55b2d1a4c53a32
ntluandt95@gmail.com <ntluandt95@gmail.com>
2023-10-09 14:01:55 -0500

    fix error

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+
 var btn_generate = document.getElementById("btn_generate");
 var inputSize = document.getElementById("input_size");
 var margin_size;
@@ -10,360 +11,330 @@ var btnQuick = document.getElementById("btnQuick");
 var btnHeap = document.getElementById("btnHeap");
 
 var inputSpeed = document.getElementById("speed");
-var speed = 500 / inputSpeed.value;
+var speed = 0.1 / inputSpeed.value;
 var input_size = inputSize.value;
+
 inputSpeed.addEventListener("change", () => {
-    speed = 500 / inputSpeed.value;
-})
-console.log(speed);
+  speed = 0.1 / inputSpeed.value;
+});
+
 inputSize.addEventListener("change", () => {
-    input_size = inputSize.value;
-    generateArray();
-})
+  input_size = inputSize.value;
+  generateArray();
+});
 
 var initSpeed = 0;
-btn_generate.addEventListener("click", generateArray);
+btn_generate.addEventListener("click", generateArrayAndRefresh);
 
 var cont = document.getElementById("array_container");
 
 var divsList = [];
 var div_sizes = [];
+
 generateArray();
+
+function generateArrayAndRefresh() {
+  location.reload();
+  generateArray();
+}
+
 function generateArray() {
+  cont.innerHTML = "";
+  divsList = [];
+  div_sizes = [];
 
-    cont.innerHTML = "";
-    for (var i = 0; i < input_size; i++) {
-        div_sizes[i] = Math.floor(Math.random() * 0.5 * (inputSize.max - inputSize.min)) + 10;
-        divsList[i] = document.createElement("div");
-        cont.appendChild(divsList[i]);
-        margin_size = 0.1;
-        divsList[i].style = " margin:0% " + margin_size + "%; background-color:blue; width:" + (100 / input_size - 2 * margin_size) + "%; height:" + (div_sizes[i]) + "%;";
-    }
+  input_size = inputSize.value;
 
+  for (var i = 0; i < input_size; i++) {
+    div_sizes[i] =
+      Math.floor(Math.random() * 0.5 * (inputSize.max - inputSize.min)) + 10;
+    divsList[i] = document.createElement("div");
+    cont.appendChild(divsList[i]);
+    margin_size = 0.1;
+    divsList[i].style =
+      " margin:0% " +
+      margin_size +
+      "%; background-color:blue; width:" +
+      (100 / input_size - 2 * margin_size) +
+      "%; height:" +
+      div_sizes[i] +
+      "%;";
+  }
 }
+
 function swap(a, b) {
-
-    // setTimeout(() => {
-    //     temp = div_sizes[a];
-    //     div_sizes[a] = div_sizes[b];
-    //     div_sizes[b] = temp;
-    //     divUpdate(divsList[a], div_sizes[a], "yellow");
-    //     divUpdate(divsList[b], div_sizes[b], "yellow");
-    //     divUpdate(divsList[a], div_sizes[a], "blue");
-    //     divUpdate(divsList[b], div_sizes[b], "blue");
-    // }, speed += 10);
-    divUpdate(divsList[a], div_sizes[a], "yellow");
-    temp = div_sizes[a];
-    div_sizes[a] = div_sizes[b];
-    div_sizes[b] = temp;
-    divUpdate(divsList[a], div_sizes[a], "red");
-    divUpdate(divsList[b], div_sizes[b], "red");
-
-
-
+  divUpdate(divsList[a], div_sizes[a], "yellow");
+  temp = div_sizes[a];
+  div_sizes[a] = div_sizes[b];
+  div_sizes[b] = temp;
+  divUpdate(divsList[a], div_sizes[a], "red");
+  divUpdate(divsList[b], div_sizes[b], "red");
 }
-
 
 function divUpdate(element, height, color) {
-    // element.style = " margin:0% " + margin_size + "%; width:" + (100 / input_size.value - (2 * margin_size)) + "%; height:" + height + "%; background-color:" + color + ";";
-    window.setTimeout(
-        () => {
-            element.style = " margin:0% " + margin_size + "%; width:" + (100 / input_size - 2 * margin_size) + "%; height:" + height + "%; background-color:" + color + ";";
-        }, initSpeed += speed);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      element.style =
+        " margin:0% " +
+        margin_size +
+        "%; width:" +
+        (100 / input_size - 2 * margin_size) +
+        "%; height:" +
+        height +
+        "%; background-color:" +
+        color +
+        ";";
+      resolve();
+    }, (initSpeed += speed));
+  });
 }
 
+async function bubbleSort() {
+  for (let i = 0; i < input_size; i++) {
+    for (let j = 0; j < input_size - i - 1; j++) {
+      await divUpdate(divsList[j], div_sizes[j], "yellow");
 
+      if (div_sizes[j] > div_sizes[j + 1]) {
+        await divUpdate(divsList[j], div_sizes[j], "red");
+        await divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
 
-btnBubble.addEventListener("click", () => {
-    for (let i = 0; i < input_size; i++) {
-        for (let j = 0; j < input_size - i - 1; j++) {
-            divUpdate(divsList[j], div_sizes[j], "yellow");
+        var temp = div_sizes[j];
+        div_sizes[j] = div_sizes[j + 1];
+        div_sizes[j + 1] = temp;
 
-            if (div_sizes[j] > div_sizes[j + 1]) {
-
-                divUpdate(divsList[j], div_sizes[j], "red");
-                divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
-                // swap(j, j + 1);
-                var temp = div_sizes[j];
-                div_sizes[j] = div_sizes[j + 1];
-                div_sizes[j + 1] = temp;
-
-                divUpdate(divsList[j], div_sizes[j], "red");
-                divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
-
-            }
-            divUpdate(divsList[j], div_sizes[j], "blue");
-            divUpdate(divsList[j + 1], div_sizes[j + 1], "green");
-
-            //console.log(div_sizes);
-        }
-
+        await divUpdate(divsList[j], div_sizes[j], "red");
+        await divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
+      }
+      await divUpdate(divsList[j], div_sizes[j], "blue");
+      await divUpdate(divsList[j + 1], div_sizes[j + 1], "green");
     }
-    divUpdate(divsList[0], div_sizes[0], "green");
-})
+  }
+  await divUpdate(divsList[0], div_sizes[0], "green");
+}
 
-btnSelection.addEventListener("click", () => {
-    // divUpdate(divsList[0],div_sizes[0],"yellow");
-    for (let i = 0; i < input_size - 1; i++) {
-
-        divUpdate(divsList[i], div_sizes[i], "yellow");
-        var minIndex = i;
-        for (let j = i + 1; j < input_size; j++) {
-            divUpdate(divsList[minIndex], div_sizes[minIndex], "yellow");
-
-            if (div_sizes[minIndex] > div_sizes[j]) {
-
-                divUpdate(divsList[minIndex], div_sizes[minIndex], "blue");
-                minIndex = j;
-
-            }
-
-
-
-
-        }
-
-        var temp = div_sizes[i];
-        div_sizes[i] = div_sizes[minIndex];
-        div_sizes[minIndex] = temp;
-
-        divUpdate(divsList[i], div_sizes[i], "red");
-        divUpdate(divsList[minIndex], div_sizes[minIndex], "red");
-
-        divUpdate(divsList[i], div_sizes[i], "green");
-    }
-    divUpdate(divsList[input_size - 1], div_sizes[input_size - 1], "green");
-})
-
-btnInsertion.addEventListener("click", () => {
-
-    var i, j, key;
-    for (let i = 0; i < input_size; i++) {
-        key = div_sizes[i];
-        divUpdate(divsList[i], div_sizes[i], "yellow");
-        j = i - 1;
-        while (j >= 0 && div_sizes[j] > key) {
-            divUpdate(divsList[j], div_sizes[j], "red");
-            divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
-            div_sizes[j + 1] = div_sizes[j];
-            divUpdate(divsList[j], div_sizes[j], "red");
-            divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
-            divUpdate(divsList[j], div_sizes[j], "blue");
-            if (j == (i - 1)) {
-                divUpdate(divsList[j + 1], div_sizes[j + 1], "yellow");
-            }
-            else {
-                divUpdate(divsList[j + 1], div_sizes[j + 1], "blue");
-            }
-            j = j - 1;
-        }
-        div_sizes[j + 1] = key;
-        for (let index = 0; index < i; index++) {
-            divUpdate(divsList[index], div_sizes[index], "green");
-
-        }
-
-    }
-})
-
-btnMerge.addEventListener("click", () => {
-    mergeSort(0, input_size - 1);
+btnBubble.addEventListener("click", async () => {
+  await bubbleSort();
 });
 
-function merge(l, m, r) {
-    var n1 = m - l + 1;
-    var n2 = r - m;
+btnSelection.addEventListener("click", async () => {
+  for (let i = 0; i < input_size - 1; i++) {
+    await divUpdate(divsList[i], div_sizes[i], "yellow");
+    var minIndex = i;
+    for (let j = i + 1; j < input_size; j++) {
+      await divUpdate(divsList[minIndex], div_sizes[minIndex], "yellow");
 
-
-    var L = [];
-    var R = [];
-    for (let i = 0; i < n1; i++) {
-        L[i] = div_sizes[l + i];
+      if (div_sizes[minIndex] > div_sizes[j]) {
+        await divUpdate(divsList[minIndex], div_sizes[minIndex], "blue");
+        minIndex = j;
+      }
     }
 
-    for (let j = 0; j < n2; j++) {
-        R[j] = div_sizes[m + 1 + j];
+    var temp = div_sizes[i];
+    div_sizes[i] = div_sizes[minIndex];
+    div_sizes[minIndex] = temp;
+
+    await divUpdate(divsList[i], div_sizes[i], "red");
+    await divUpdate(divsList[minIndex], div_sizes[minIndex], "red");
+
+    await divUpdate(divsList[i], div_sizes[i], "green");
+  }
+  await divUpdate(divsList[input_size - 1], div_sizes[input_size - 1], "green");
+});
+
+btnInsertion.addEventListener("click", async () => {
+  for (let i = 0; i < input_size; i++) {
+    let key = div_sizes[i];
+    await divUpdate(divsList[i], div_sizes[i], "yellow");
+    let j = i - 1;
+    while (j >= 0 && div_sizes[j] > key) {
+      await divUpdate(divsList[j], div_sizes[j], "red");
+      await divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
+      div_sizes[j + 1] = div_sizes[j];
+      await divUpdate(divsList[j], div_sizes[j], "red");
+      await divUpdate(divsList[j + 1], div_sizes[j + 1], "red");
+      await divUpdate(divsList[j], div_sizes[j], "blue");
+      if (j == i - 1) {
+        await divUpdate(divsList[j + 1], div_sizes[j + 1], "yellow");
+      } else {
+        await divUpdate(divsList[j + 1], div_sizes[j + 1], "blue");
+      }
+      j = j - 1;
     }
-
-    console.log(L);
-    console.log(R);
-    var i = 0;
-    var j = 0;
-    var k = l;
-
-
-    while (i < n1 && j < n2) {
-        //console.log("k:"+k+"i:"+i+"j:"+j);
-        if (L[i] <= R[j]) {
-            div_sizes[k] = L[i];
-            divUpdate(divsList[k], div_sizes[k], "green");
-
-            i++;
-        }
-        else {
-            div_sizes[k] = R[j];
-            divUpdate(divsList[k], div_sizes[k], "green");
-            j++;
-        }
-        k++;
+    div_sizes[j + 1] = key;
+    for (let index = 0; index < i; index++) {
+      await divUpdate(divsList[index], div_sizes[index], "green");
     }
+  }
+});
 
-    while (i < n1) {
-        div_sizes[k] = L[i];
-        divUpdate(divsList[k], div_sizes[k], "green");
-        i++;
-        k++;
+btnMerge.addEventListener("click", () => {
+  mergeSort(0, input_size - 1);
+});
+
+async function merge(l, m, r) {
+  let n1 = m - l + 1;
+  let n2 = r - m;
+  let L = [];
+  let R = [];
+
+  for (let i = 0; i < n1; i++) {
+    L[i] = div_sizes[l + i];
+  }
+
+  for (let j = 0; j < n2; j++) {
+    R[j] = div_sizes[m + 1 + j];
+  }
+
+  let i = 0;
+  let j = 0;
+  let k = l;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      div_sizes[k] = L[i];
+      await divUpdate(divsList[k], div_sizes[k], "green");
+      i++;
+    } else {
+      div_sizes[k] = R[j];
+      await divUpdate(divsList[k], div_sizes[k], "green");
+      j++;
     }
-    while (j < n2) {
-        div_sizes[k] = R[j];
-        divUpdate(divsList[k], div_sizes[k], "green");
-        j++;
-        k++;
-    }
+    k++;
+  }
+
+  while (i < n1) {
+    div_sizes[k] = L[i];
+    await divUpdate(divsList[k], div_sizes[k], "green");
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    div_sizes[k] = R[j];
+    await divUpdate(divsList[k], div_sizes[k], "green");
+    j++;
+    k++;
+  }
 }
 
-function mergeSort(l, r) {
-    if (l >= r) {
-        return;
-        console.log(div_sizes);
-    }
-    var m = Math.floor(l + (r - l) / 2);
-    divUpdate(divsList[l], div_sizes[l], "yellow");
-    mergeSort(l, m);
-    mergeSort(m + 1, r);
-    merge(l, m, r);
+async function mergeSort(l, r) {
+  if (l >= r) {
+    return;
+  }
+  let m = Math.floor(l + (r - l) / 2);
+  await divUpdate(divsList[l], div_sizes[l], "yellow");
+  await mergeSort(l, m);
+  await mergeSort(m + 1, r);
+  await merge(l, m, r);
 }
-
 
 btnQuick.addEventListener("click", () => {
-    
-    quickSort(0, input_size-1);
-    
-})
+  quickSort(0, input_size - 1);
+});
 
-function quickSort(low, high) {
-    if (low < high) {
-        var pi = partition(low, high);
-        quickSort(low, pi - 1);
-        quickSort(pi + 1, high);
-    }
+async function quickSort(low, high) {
+  if (low < high) {
+    let pi = await partition(low, high);
+    await quickSort(low, pi - 1);
+    await quickSort(pi + 1, high);
+  }
 }
 
-function partition(low, high) {
-    var pivot = div_sizes[low];
-    divUpdate(divsList[low], div_sizes[low], "yellow");
-    var i = low + 1;
-    for (let j = low+1; j <= high; j++) {
-        if (div_sizes[j] < pivot) {
-            divUpdate(divsList[j], div_sizes[j], "yellow");
-            divUpdate(divsList[i], div_sizes[i], "red");
-            divUpdate(divsList[j], div_sizes[j], "red");
-
-            
-            var temp = div_sizes[i];
-            div_sizes[i] = div_sizes[j];
-            div_sizes[j] = temp;
-
-            divUpdate(divsList[i], div_sizes[i], "red");
-            divUpdate(divsList[j], div_sizes[j], "red");
-            divUpdate(divsList[i], div_sizes[i], "blue");
-            divUpdate(divsList[j], div_sizes[j], "blue");
-            i++;
-        }
-        
+async function partition(low, high) {
+  let pivot = div_sizes[low];
+  await divUpdate(divsList[low], div_sizes[low], "yellow");
+  let i = low + 1;
+  for (let j = low + 1; j <= high; j++) {
+    if (div_sizes[j] < pivot) {
+      await divUpdate(divsList[j], div_sizes[j], "yellow");
+      await divUpdate(divsList[i], div_sizes[i], "red");
+      await divUpdate(divsList[j], div_sizes[j], "red");
+      let temp = div_sizes[i];
+      div_sizes[i] = div_sizes[j];
+      div_sizes[j] = temp;
+      await divUpdate(divsList[i], div_sizes[i], "red");
+      await divUpdate(divsList[j], div_sizes[j], "red");
+      await divUpdate(divsList[i], div_sizes[i], "blue");
+      await divUpdate(divsList[j], div_sizes[j], "blue");
+      i++;
     }
-    divUpdate(divsList[low], div_sizes[low], "red");
-    divUpdate(divsList[i-1], div_sizes[i-1], "red");
-
-    var temp = div_sizes[i-1];
-    div_sizes[i-1] = div_sizes[low];
-    div_sizes[low] = temp;
-
-    divUpdate(divsList[low], div_sizes[low], "red");
-    divUpdate(divsList[i-1], div_sizes[i-1], "red");
-
-    for(let t = low; t<=i;t++){
-        divUpdate(divsList[t], div_sizes[t], "green");
-    
-    }
-    return i-1;
+  }
+  await divUpdate(divsList[low], div_sizes[low], "red");
+  await divUpdate(divsList[i - 1], div_sizes[i - 1], "red");
+  let temp = div_sizes[i - 1];
+  div_sizes[i - 1] = div_sizes[low];
+  div_sizes[low] = temp;
+  await divUpdate(divsList[low], div_sizes[low], "red");
+  await divUpdate(divsList[i - 1], div_sizes[i - 1], "red");
+  for (let t = low; t <= i; t++) {
+    await divUpdate(divsList[t], div_sizes[t], "green");
+  }
+  return i - 1;
 }
 
 btnHeap.addEventListener("click", () => {
-    heap_sort();
-})
+  heap_sort();
+});
 
-function swap(i,j)
-{
-    divUpdate(divsList[i],div_sizes[i],"red");//Color update
-    divUpdate(divsList[j],div_sizes[j],"red");//Color update
+function swap(i, j) {
+  divUpdate(divsList[i], div_sizes[i], "red");
+  divUpdate(divsList[j], div_sizes[j], "red");
 
-    var temp=div_sizes[i];
-    div_sizes[i]=div_sizes[j];
-    div_sizes[j]=temp;
+  let temp = div_sizes[i];
+  div_sizes[i] = div_sizes[j];
+  div_sizes[j] = temp;
 
-    divUpdate(divsList[i],div_sizes[i],"red");//Height update
-    divUpdate(divsList[j],div_sizes[j],"red");//Height update
+  divUpdate(divsList[i], div_sizes[i], "red");
+  divUpdate(divsList[j], div_sizes[j], "red");
 
-    divUpdate(divsList[i],div_sizes[i],"blue");//Color update
-    divUpdate(divsList[j],div_sizes[j],"blue");//Color update
+  divUpdate(divsList[i], div_sizes[i], "blue");
+  divUpdate(divsList[j], div_sizes[j], "blue");
 }
 
-function max_heapify(n,i)
-{
-    var largest=i;
-    var l=2*i+1;
-    var r=2*i+2;
+function max_heapify(n, i) {
+  let largest = i;
+  let l = 2 * i + 1;
+  let r = 2 * i + 2;
 
-    if(l<n && div_sizes[l]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            divUpdate(divsList[largest],div_sizes[largest],"blue");//Color update
-        }
-
-        largest=l;
-
-        divUpdate(divsList[largest],div_sizes[largest],"red");//Color update
+  if (l < n && div_sizes[l] > div_sizes[largest]) {
+    if (largest != i) {
+      divUpdate(divsList[largest], div_sizes[largest], "blue");
     }
 
-    if(r<n && div_sizes[r]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            divUpdate(divsList[largest],div_sizes[largest],"blue");//Color update
-        }
+    largest = l;
 
-        largest=r;
+    divUpdate(divsList[largest], div_sizes[largest], "red");
+  }
 
-        divUpdate(divsList[largest],div_sizes[largest],"red");//Color update
+  if (r < n && div_sizes[r] > div_sizes[largest]) {
+    if (largest != i) {
+      divUpdate(divsList[largest], div_sizes[largest], "blue");
     }
 
-    if(largest!=i)
-    {
-        swap(i,largest);
+    largest = r;
 
-        max_heapify(n,largest);
-    }
+    divUpdate(divsList[largest], div_sizes[largest], "red");
+  }
+
+  if (largest != i) {
+    swap(i, largest);
+
+    max_heapify(n, largest);
+  }
 }
 
-function heap_sort()
-{
-    for(var i=Math.floor(input_size/2)-1;i>=0;i--)
-    {
-        max_heapify(input_size,i);
-    }
+async function heap_sort() {
+  for (let i = Math.floor(input_size / 2) - 1; i >= 0; i--) {
+    await max_heapify(input_size, i);
+  }
 
-    for(var i=input_size-1;i>0;i--)
-    {
-        swap(0,i);
-        divUpdate(divsList[i],div_sizes[i],"green");//Color update
-        divUpdate(divsList[i],div_sizes[i],"yellow");//Color update
-
-        max_heapify(i,0);
-
-        divUpdate(divsList[i],div_sizes[i],"blue");//Color update
-        divUpdate(divsList[i],div_sizes[i],"green");//Color update
-    }
-    divUpdate(divsList[i],div_sizes[i],"green");//Color update
+  for (let i = input_size - 1; i > 0; i--) {
+    swap(0, i);
+    await divUpdate(divsList[i], div_sizes[i], "green");
+    await divUpdate(divsList[i], div_sizes[i], "yellow");
+    await max_heapify(i, 0);
+    await divUpdate(divsList[i], div_sizes[i], "blue");
+    await divUpdate(divsList[i], div_sizes[i], "green");
+  }
+  await divUpdate(divsList[0], div_sizes[0], "green");
 }
